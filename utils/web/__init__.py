e67741229f0ca9dc94496f3b2d92905e4b270ec8
meiyujack <meiyujack@msn.cn>
2023-02-02 17:05:39 +0800

    fix error and achieve the goal

--- utils/web/__init__.py
+++ utils/web/__init__.py
@@ -3,19 +3,26 @@ from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
 
 class WebSecurity:
 
-    def __init__(self, secret_key):
+    def __init__(self, info: dict, secret_key, max_age):
+        self.info = info
         self.serializer = URLSafeTimedSerializer(secret_key)
+        self.max_age = max_age
 
-    def generate_token(self,info):
-        token = self.serializer.dumps(info)
+    def generate_token(self):
+        token = self.serializer.dumps(self.info)
         return token
 
     def get_info_by_token(self, token):
+        """
+        加密内容字典长度为1直接返回其内容（value），否则返回元组((),()...)
+        :param token: 待解析内容
+        :return: str|tuple|None
+        """
         try:
-            info = self.serializer.loads(token)
+            info = self.serializer.loads(token, max_age=self.max_age)
         except BadSignature or SignatureExpired:
             return None
-        return info
+        return info["".join(self.info.keys())] if len(self.info) == 1 else tuple(self.info.items())
 
     def check_token(self, token):
         try:
