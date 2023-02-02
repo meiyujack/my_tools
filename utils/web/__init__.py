e8ee117035b541ca4e75f503f07c82f353d7cfc3
meiyujack <meiyujack@msn.cn>
2023-02-02 15:40:51 +0800

    fix error

--- utils/web/__init__.py
+++ utils/web/__init__.py
@@ -3,20 +3,19 @@ from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
 
 class WebSecurity:
 
-    def __init__(self, secret_key, info):
-        self.info = info
+    def __init__(self, secret_key):
         self.serializer = URLSafeTimedSerializer(secret_key)
 
-    def generate_token(self):
-        token = self.serializer.dumps(self.info)
+    def generate_token(self,info):
+        token = self.serializer.dumps(info)
         return token
 
     def get_info_by_token(self, token):
         try:
-            payload = self.serializer.loads(token)
+            info = self.serializer.loads(token)
         except BadSignature or SignatureExpired:
             return None
-        return payload
+        return info
 
     def check_token(self, token):
         try:
