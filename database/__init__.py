7fa0c4268e05ae3336d71b0973689e3b3bde8264
meiyujack <meiyujack@msn.cn>
2023-02-08 10:08:22 +0800

    support sqlite asynchronously and make sure import current server name

--- database/__init__.py
+++ database/__init__.py
@@ -1,3 +1,6 @@
+import sqlite3
+
+
 class Database:
     def __init__(self, server, **kwargs):
         """
@@ -8,46 +11,44 @@ class Database:
         """
         self.server = server
         self.basic = kwargs
+        self.conn = None
 
     def connect_db(self):
         """
         Connect to the database.
-        @return: None, except for error message.
         """
         try:
-            if self.server == "aiosqlite":
-                await self.server.connect(self.basic['file_address'])
-            return self.server.connect(
-                user=self.basic['user'],
-                password=self.basic['password'],
-                host=self.basic['host'],
-                port=self.basic['port'],
-                database=self.basic['database']
-            )
+            if self.server == sqlite3:
+                self.conn = self.server.connect(self.basic['file_address'])
+            else:
+                self.conn = self.server.connect(
+                    user=self.basic['user'],
+                    password=self.basic['password'],
+                    host=self.basic['host'],
+                    port=self.basic['port'],
+                    database=self.basic['database']
+                )
         except self.server.Error as e:
             return e
 
-    def init_table(self, conn):
+    def init_table(self):
         """
         Initialize table schema structure.
-        @param conn: database's Connection.
         """
         with open(self.basic["sql_address"], mode="r") as file:
-            conn.cursor().executescript(file.read())
-            conn.commit()
+            self.conn.cursor().executescript(file.read())
+            self.conn.commit()
             print("Initialize database completed.")
 
-    def select_db(self, conn, table, get='*', prep=None, **condition):
+    def select_db(self, table, get='*', prep=None, **condition):
         """
         Just execute "select" sentence.
-        @param conn: database's Connection.
         @param table: str, table's name.
         @param get: str, default: *, what column or property you want to get.
         @param prep: str, default:None, prep. "and" or "or(secondly)" in WHERE clause. At most two conditions now.
         @param condition: **condition, according to what condition you want to find.
         @return: rows if execute select sentence or error message.
         """
-        cursor = conn.cursor()
         sql = f"SELECT {get} FROM {table}"
         where = f" WHERE {','.join(condition.keys())}={','.join(['?'])}"
         if len(condition) == 1:
@@ -57,20 +58,19 @@ class Database:
                 [f"{''.join(m.keys())}={''.join(['?'])}" for m in [{i: j} for i, j in condition.items()]])
             sql += ' WHERE ' + where
         try:
-            cursor.execute(sql, tuple(condition.values()))
+            cursor=self.conn.execute(sql, tuple(condition.values()))
             rows = cursor.fetchall()
             if rows:
                 return rows
             # else:
             #     self.conn.commit()
         except self.server.Error as e:
-            conn.rollback()
+            self.conn.rollback()
             return f"Error: {e}"
 
-    def upsert(self, conn, table, data, constraint: int = None):
+    def upsert(self, table, data, constraint: int = None):
         """
         insert or update data into table in database.
-        @param conn: database's Connection.
         @param table: str, table's name.
         @param data: dict, data's form.
         @param constraint: int, primary key's index of data, alternative, especially for sqlite3's update sentence.
@@ -85,9 +85,8 @@ class Database:
             else:
                 sql = f'INSERT INTO {table} ({keys}) VALUES ({values}) ON DUPLICATE KEY UPDATE'
             sql += update
-            print(sql)
-            if conn.execute(sql, tuple(data.values()) * 2):
-                conn.commit()
+            if self.conn.execute(sql, tuple(data.values()) * 2):
+                self.conn.commit()
         except self.server.Error as e:
-            conn.rollback()
+            self.conn.rollback()
             return f"Error: {e}"
