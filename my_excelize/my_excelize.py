96647d6b40657a177ed608a6d57563e9838f48e2
meiyujack <meiyujack@msn.cn>
2023-10-09 21:39:45 +0800

    upload and write excel completed

--- my_excelize/my_excelize.py
+++ my_excelize/my_excelize.py
@@ -1,17 +1,21 @@
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook
 from apiflask import APIFlask, Schema
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
 from apiflask.fields import String, File
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 
 import os
 
 
 class Excelize:
-    def __init__(self, file_address, sheet_name=None):
-        self.file_address = file_address
+    def __init__(self, file_address="upload/model.xlsx", sheet_name=None):
+        if os.path.exists(file_address):
+            self.file_address = file_address
+        else:
+            wb = Workbook()
+            ws = wb.active
+            ws["A1"] = 0
+            wb.save("upload/model.xlsx")
+            self.file_address = "upload/model.xlsx"
         self.workbook = load_workbook(self.file_address)
         self.sheet = (
             self.workbook.active if not sheet_name else self.workbook[sheet_name]
@@ -43,10 +47,13 @@ my_excel.secret_key = "secret key"
 
 @my_excel.get("/")
 def index():
-    return "欢迎来到填表网站"
+    flag = False
+    if Excelize("upload/model.xlsx").get_basic()[0] > 1:
+        flag = True
+    return render_template("index.html", flag=flag)
 
 
-@my_excel.get("/model")
+@my_excel.get("/upload")
 def upload():
     return render_template("upload.html")
 
@@ -54,15 +61,15 @@ def upload():
 @my_excel.post("/model")
 @my_excel.input(InputFile, location="files")
 def upload_model(files_data):
-    print(files_data["model"], type(files_data["model"]))
     with open("upload/model.xlsx", "wb") as f:
         f.write(files_data["model"].read())
-    return "上传模板文件成功"
+    return ""
 
 
 @my_excel.get("/write")
 def write_show():
-    if not os.path.exists("upload/model.xlsx"):
+    if Excelize("upload/model.xlsx").get_th() == [0]:
+        flash("当前没有模板，请先上传含表头的excel文件（xlsx）")
         return redirect(url_for("upload"))
     excel = Excelize("upload/model.xlsx")
     th = excel.get_th()
@@ -78,11 +85,21 @@ def write_show():
     location="form",
 )
 def write_post(form_data):
-    if not os.path.exists("upload/model.xlsx"):
-        return redirect(url_for("upload"))
     excel = Excelize("upload/model.xlsx")
     content = []
     for v in form_data.values():
         content.append(v)
+
     excel.write_data(content)
     return "数据写入成功！"
+
+
+@my_excel.get("/read")
+def read_show():
+    return render_template("read.html")
+
+
+@my_excel.post("/read")
+def read():
+    excel = Excelize("upload/model.xlsx")
+    excel.get_basic()
