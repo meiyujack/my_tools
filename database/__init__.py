295090e0122b0161af8bc331d6cbb954770fe89d
meiyujack <meiyujack@msn.cn>
2023-11-11 15:50:48 +0800

    now support mysql select sentence, tested

--- database/__init__.py
+++ database/__init__.py
@@ -2,11 +2,12 @@ import sqlite3
 
 
 class Database:
+
     def __init__(self, server, **kwargs):
         """
         Initialize basic information about current database.
         @param server: database name, like mariadb, sqlite, etc.
-        @param kwargs: key=value. if sqlite,file_address and sql_address in case want to reconstruct table;
+        @param kwargs: key=value. if sqlite, file_address, and sql_address in case want to reconstruct table;
         if mariadb, host={host}, user={user}, etc.
         """
         self.server = server
@@ -26,8 +27,7 @@ class Database:
                     password=self.basic['password'],
                     host=self.basic['host'],
                     port=self.basic['port'],
-                    database=self.basic['database']
-                )
+                    database=self.basic['database'])
         except self.server.Error as e:
             return e
 
@@ -50,66 +50,73 @@ class Database:
         @return: rows if execute select sentence or error message.
         """
         sql = f"SELECT {get} FROM {table}"
-        if condition:
-            where = f" WHERE {','.join(condition.keys())}={','.join(['?'])}"
-            if len(condition) == 1:
-                sql += where
-            else:
-                where = f' {prep} '.join(
-                    [f"{''.join(m.keys())}={''.join(['?'])}" for m in [{i: j} for i, j in condition.items()]])
-                sql += ' WHERE ' + where
         try:
-            cursor = self.conn.execute(sql, tuple(condition.values()))
-            rows = cursor.fetchall()
+            if condition:
+                where = f" WHERE {','.join(condition.keys())}={','.join(['?'])}" if self.server == sqlite3 else f" WHERE {','.join(condition.keys())}={','.join(['%s'])}"
+                if len(condition) == 1:
+                    sql += where
+                else:
+                    where = f' {prep} '.join([
+                        f"{''.join(m.keys())}={''.join(['?'])}" for m in [{
+                            i: j
+                        } for i, j in condition.items()]
+                    ])
+                    sql += ' WHERE ' + where
+            if self.server == sqlite3:
+                cursor = self.conn.execute(sql, tuple(condition.values()))
+                rows = cursor.fetchall()
+            else:
+                with self.conn:
+                    with self.conn.cursor() as cursor:
+                        cursor.execute(sql, tuple(condition.values()))
+                        rows = cursor.fetchall()
             if rows:
                 return rows
-            # else:
-            #     self.conn.commit()
         except self.server.Error as e:
             self.conn.rollback()
             return f"Error: {e}"
 
-    def insert(self,table,data):
+    def insert(self, table, data):
         self.connect_db()
         keys = ','.join(data.keys())
         values = ','.join(['?'] * len(data))
         try:
-            sql=f"INSERT INTO {table}({keys}) VALUES({values});"
+            sql = f"INSERT INTO {table}({keys}) VALUES({values});"
             if self.conn.execute(sql, tuple(data.values())):
                 self.conn.commit()
         except self.server.Error as ex:
             self.conn.rollback()
             return f"Error:{ex}"
 
-    def update(self,table,data,**condition):
+    def update(self, table, data, **condition):
         self.connect_db()
-        s=""
-        for k,v in data.items():
-            s+=f"{k}='{v}',"
-        s=s[:-1]
-        sql=f"UPDATE {table} SET {s}"
-        where=f" WHERE {','.join(condition.keys())}={','.join(['?'])};"
-        sql+=where
+        s = ""
+        for k, v in data.items():
+            s += f"{k}='{v}',"
+        s = s[:-1]
+        sql = f"UPDATE {table} SET {s}"
+        where = f" WHERE {','.join(condition.keys())}={','.join(['?'])};"
+        sql += where
         try:
-            if self.conn.execute(sql,tuple(condition.values())):
+            if self.conn.execute(sql, tuple(condition.values())):
                 self.conn.commit()
         except self.server.Error as ex:
             self.conn.rollback()
             return f"Error:{ex}"
 
-    def delete(self,table,**condition):
+    def delete(self, table, **condition):
         self.connect_db()
-        sql=f"DELETE FROM {table}"
-        where=f" WHERE {','.join(condition.keys())}={','.join(['?'])};"
-        sql+=where
+        sql = f"DELETE FROM {table}"
+        where = f" WHERE {','.join(condition.keys())}={','.join(['?'])};"
+        sql += where
         try:
-            if self.conn.execute(sql,tuple(condition.values())):
+            if self.conn.execute(sql, tuple(condition.values())):
                 self.conn.commit()
         except self.server.Error as ex:
             self.conn.rollback()
             return f"Error:{ex}"
 
-    def just_exe(self,sql):
+    def just_exe(self, sql):
         try:
             with self.conn.execute(sql) as cursor:
                 return cursor.fetchall()
